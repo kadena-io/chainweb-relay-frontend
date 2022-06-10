9e9511aeeb983b02c302a499fa4c6a13d282cda9
ggobugi27 <ggobugi27@github.com>
2022-06-10 17:37:38 +0900

    debut time / timep

--- src/components/BondInfo.js
+++ src/components/BondInfo.js
@@ -1,45 +1,71 @@
-import React, { useState, useContext, useEffect } from 'react';
-import '../App.css';
-import { Modal, Form, Message, Icon, List, Input, Label, Segment } from 'semantic-ui-react';
-import Button from '../wallet/components/shared/Button';
+import React, { useState, useContext, useEffect } from "react";
+import "../App.css";
+import {
+  Modal,
+  Form,
+  Message,
+  Icon,
+  List,
+  Input,
+  Label,
+  Segment,
+} from "semantic-ui-react";
+import Button from "../wallet/components/shared/Button";
 import { PactContext } from "../contexts/PactContext";
-import { WalletContext } from "../wallet/contexts/WalletContext"
-import Pact from 'pact-lang-api';
-import SimpleSign from './SimpleSign'
-import Rotate from './Rotate'
+import { WalletContext } from "../wallet/contexts/WalletContext";
+import Pact from "pact-lang-api";
+import SimpleSign from "./SimpleSign";
+import Rotate from "./Rotate";
 
 const BondInfo = (props) => {
   const pact = useContext(PactContext);
   const wallet = useContext(WalletContext);
-  const [open, setOpen] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
   const [key, setKey] = React.useState(false);
 
-  const {bond, bondExist} = props;
+  const { bond, bondExist } = props;
+  const bondDate = bond.bond.date
+    ? bond.bond.date.time
+      ? bond.bond.date.time
+      : bond.bond.date.timep
+    : "";
   const closeDetails = () => {
     setOpen(false);
-  }
+  };
 
-  let {unlock, lockup} = pact.poolInfo;
+  let { unlock, lockup } = pact.poolInfo;
 
-  unlock = unlock? unlock.int : unlock;
-  lockup = lockup? lockup.int : lockup;
+  unlock = unlock ? unlock.int : unlock;
+  lockup = lockup ? lockup.int : lockup;
 
   const addDay = (date, add) => {
     let newDate = new Date(date);
-    newDate.setDate(newDate.getDate())
-    newDate = newDate.setDate(newDate.getDate() + add)
+    newDate.setDate(newDate.getDate());
+    newDate = newDate.setDate(newDate.getDate() + add);
     return new Date(newDate).toISOString();
-  }
-  const renewable = bond.bond.date && unlock && addDay(bond.bond.date.timep, lockup)
-    ? (new Date(addDay(bond.bond.date.timep, lockup))<new Date() && new Date() < new Date(addDay(bond.bond.date.timep, lockup+unlock))) : false;
-  const unbondable = bond.bond.date && unlock && addDay(bond.bond.date.timep, lockup+unlock)
-    ? new Date(addDay(bond.bond.date.timep, lockup+unlock))< new Date() : false;
-  const active = bond.bond.date && unlock && addDay(bond.bond.date.timep, lockup)
-    ? (new Date(addDay(bond.bond.date.timep, lockup))>new Date()) : false;
+  };
+  const renewable =
+    bond.bond.date && unlock && addDay(bondDate, lockup)
+      ? new Date(addDay(bondDate, lockup)) < new Date() &&
+        new Date() < new Date(addDay(bondDate, lockup + unlock))
+      : false;
+  const unbondable =
+    bond.bond.date && unlock && addDay(bondDate, lockup + unlock)
+      ? new Date(addDay(bondDate, lockup + unlock)) < new Date()
+      : false;
+  const active =
+    bond.bond.date && unlock && addDay(bondDate, lockup)
+      ? new Date(addDay(bondDate, lockup)) > new Date()
+      : false;
 
-  return(
-    <Segment style={{backgroundColor: "#DCDDDE"}}>
-      <div role="list" className="ui divided list" style={{margin: "10px"}} key={bond.key}>
+  return (
+    <Segment style={{ backgroundColor: "#DCDDDE" }}>
+      <div
+        role="list"
+        className="ui divided list"
+        style={{ margin: "10px" }}
+        key={bond.key}
+      >
         <div className="bheader">
           <h3>Bond Detail</h3>
         </div>
@@ -48,7 +74,10 @@ const BondInfo = (props) => {
             <tbody>
               <tr className="bitem">
                 <td className="blabel">Bond Name</td>
-                <td colSpan="3" className="blong bname">{bond.key.length > 64 ? bond.key.slice(0,30)+ "..."+bond.key.slice(45): bond.key}
+                <td colSpan="3" className="blong bname">
+                  {bond.key.length > 64
+                    ? bond.key.slice(0, 30) + "..." + bond.key.slice(45)
+                    : bond.key}
                 </td>
               </tr>
               <tr className="bitem">
@@ -65,47 +94,67 @@ const BondInfo = (props) => {
               </tr>
               <tr className="bitem">
                 <td className="blabel">Activity</td>
-                <td className="bvalue">{bond.bond.activity && bond.bond.activity.int}</td>
+                <td className="bvalue">
+                  {bond.bond.activity && bond.bond.activity.int}
+                </td>
                 <td className="blabel">APY</td>
-                <td className="bvalue">{bond.bond.rate && (bond.bond.rate*100*365).toFixed(2)}%</td>
+                <td className="bvalue">
+                  {bond.bond.rate && (bond.bond.rate * 100 * 365).toFixed(2)}%
+                </td>
               </tr>
               <tr className="bitem">
                 <td className="blabel">Date</td>
-                <td className="bvalue">{bond.bond.date && bond.bond.date.timep.slice(0,19)}</td>
+                <td className="bvalue">
+                  {bond.bond.date && bondDate.slice(0, 19)}
+                </td>
                 <td className="blabel">Renew</td>
-                <td className="bvalue">{bond.bond.date && unlock && addDay(bond.bond.date.timep, lockup).slice(0,19)}</td>
+                <td className="bvalue">
+                  {bond.bond.date &&
+                    unlock &&
+                    addDay(bondDate, lockup).slice(0, 19)}
+                </td>
               </tr>
               <tr className="bitem">
                 <td className="blabel">Unbond</td>
-                <td className="bvalue">{bond.bond.date && unlock && addDay(bond.bond.date.timep, lockup + unlock).slice(0,19)}</td>
+                <td className="bvalue">
+                  {bond.bond.date &&
+                    unlock &&
+                    addDay(bondDate, lockup + unlock).slice(0, 19)}
+                </td>
                 <td className="blabel">Status</td>
-                <td className="bvalue">{bond.bond.terminated ? "terminated" : active ? "active" : "inactive"}</td>
+                <td className="bvalue">
+                  {bond.bond.terminated
+                    ? "terminated"
+                    : active
+                    ? "active"
+                    : "inactive"}
+                </td>
               </tr>
             </tbody>
           </table>
-          <br/>
+          <br />
           <SimpleSign
             disabled={bond.bond.terminated || !renewable}
             activity="Renew"
             bond={bond.key}
             bondInfo={bond.bond}
-            />
+          />
           <SimpleSign
             disabled={bond.bond.terminated || !unbondable}
             activity="Unbond"
             bond={bond.key}
             bondInfo={bond.bond}
-            />
+          />
           <Rotate
             disabled={bond.key === ""}
             activity="Rotate"
             bond={bond.key}
             bondInfo={bond.bond}
-            />
+          />
         </div>
       </div>
     </Segment>
-  )
-}
+  );
+};
 
-export default BondInfo
+export default BondInfo;
